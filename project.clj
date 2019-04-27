1c5b103928760360807856d4d5a4c324c5732d3a
Pranay Kanwar <pranay.kanwar@gmail.com>
2019-04-27 12:05:07 +0530

    Bump commons-lang3 version

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/tools.cli "0.4.2"]
                  [org.clojure/java.jmx "0.3.4"]
-                 [org.apache.commons/commons-lang3 "3.8.1"]
+                 [org.apache.commons/commons-lang3 "3.9"]
                  [cheshire "5.8.1"]]
   :main ^:skip-aot jmx-dump.core
   :target-path "target/%s"
