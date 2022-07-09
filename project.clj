1f3e34166cab2c3c12ad84a0601dc4901d551c3b
Pranay Kanwar <pranay.kanwar@gmail.com>
2022-07-09 14:31:00 +0530

    Updating dependencies

--- project.clj
+++ project.clj
@@ -3,16 +3,16 @@
   :url "https://github.com/r4um/jmx-dump"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/tools.cli "1.0.194"]
                  [org.clojure/java.jmx "1.0.0"]
                  [org.apache.commons/commons-lang3 "3.9"]
-                 [cheshire "5.10.1"]]
+                 [cheshire "5.11.0"]]
   :main jmx-dump.core
   :target-path "target/%s"
   :bin {:name          "jmx-dump"
         :jvm-opts [ "$JVM_OPTS" ]}
   :profiles {:uberjar {:aot :all}
-             :dev     {:dependencies [[clj-kondo "2021.12.01"]]
+             :dev     {:dependencies [[clj-kondo "2022.06.22"]]
                        :plugins      [[lein-binplus "0.6.6"]]}}
   :aliases {"clj-kondo" ["run" "-m" "clj-kondo.main" "--lint" "src"]})
