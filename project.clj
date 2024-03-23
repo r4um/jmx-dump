2010d6e0a91feffd3fda75e646d0771c06702ae6
Pranay Kanwar <pranay.kanwar@gmail.com>
2024-03-23 17:21:19 +0530

    Bump dependencies

--- project.clj
+++ project.clj
@@ -3,15 +3,15 @@
   :url "https://github.com/r4um/jmx-dump"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.11.2"]
                  [org.clojure/tools.cli "1.0.194"]
                  [org.clojure/java.jmx "1.0.0"]
                  [org.apache.commons/commons-lang3 "3.9"]
-                 [cheshire "5.11.0"]]
+                 [cheshire "5.12.0"]]
   :main jmx-dump.core
   :bin {:name          "jmx-dump"
         :jvm-opts [ "$JVM_OPTS" ]}
   :profiles {:uberjar {:aot :all}
-             :dev     {:dependencies [[clj-kondo "2023.01.20"]]
+             :dev     {:dependencies [[clj-kondo "2024.03.13"]]
                        :plugins      [[lein-binplus "0.6.6"]]}}
   :aliases {"clj-kondo" ["run" "-m" "clj-kondo.main" "--lint" "src"]})
