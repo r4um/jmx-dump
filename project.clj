e210736e604af866b0dd3aab28ec27af4296ff27
Pranay Kanwar <pranay.kanwar@gmail.com>
2024-09-28 13:52:07 +0530

    Move to clojure 1.12 and bump deps

--- project.clj
+++ project.clj
@@ -1,17 +1,17 @@
-(defproject jmx-dump "0.12.0"
+(defproject jmx-dump "0.13.0"
   :description "Dump JMX Metrics"
   :url "https://github.com/r4um/jmx-dump"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.11.2"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
                  [org.clojure/tools.cli "1.0.194"]
                  [org.clojure/java.jmx "1.0.0"]
                  [org.apache.commons/commons-lang3 "3.9"]
-                 [cheshire "5.12.0"]]
+                 [cheshire "5.13.0"]]
   :main jmx-dump.core
   :bin {:name          "jmx-dump"
-        :jvm-opts [ "$JVM_OPTS" ]}
+        :jvm-opts ["$JVM_OPTS"]}
   :profiles {:uberjar {:aot :all}
-             :dev     {:dependencies [[clj-kondo "2024.03.13"]]
+             :dev     {:dependencies [[clj-kondo "2024.08.29"]]
                        :plugins      [[lein-binplus "0.6.8"]]}}
   :aliases {"clj-kondo" ["run" "-m" "clj-kondo.main" "--lint" "src"]})
