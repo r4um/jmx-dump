b819aed7513071479feaa83a61fe0a0c046077d3
Pranay Kanwar <pranay.kanwar@gmail.com>
2025-05-18 12:45:04 +0530

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,15 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.12.0"]
-                 [org.clojure/tools.cli "1.0.194"]
-                 [org.clojure/java.jmx "1.0.0"]
-                 [org.apache.commons/commons-lang3 "3.9"]
-                 [cheshire "5.13.0"]]
+                 [org.clojure/tools.cli "1.1.230"]
+                 [org.clojure/java.jmx "1.1.0"]
+                 [org.apache.commons/commons-lang3 "3.17.0"]
+                 [cheshire "6.0.0"]]
   :main jmx-dump.core
   :bin {:name          "jmx-dump"
         :jvm-opts ["$JVM_OPTS"]}
   :profiles {:uberjar {:aot :all}
-             :dev     {:dependencies [[clj-kondo "2024.08.29"]]
+             :dev     {:dependencies [[clj-kondo "2025.04.07"]]
                        :plugins      [[lein-binplus "0.6.8"]
                                       [lein-ancient "1.0.0-RC3"]]}}
   :aliases {"clj-kondo" ["run" "-m" "clj-kondo.main" "--lint" "src"]})
